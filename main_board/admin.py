f1a38b7019380edf450e64df55d2e3f0324053bb
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-07-31 20:21:31 +0800

    Defined MarketIndex and ValueUpdate models

--- main_board/admin.py
+++ main_board/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from main_board.models import MarketIndex, ValueUpdate
+
+
+class MarketIndexAdmin(admin.ModelAdmin):
+    fields = ['ticker', 'name']
+
+
+admin.site.register(MarketIndex, MarketIndexAdmin)
+admin.site.register(ValueUpdate)
--- main_board/models.py
+++ main_board/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class MarketIndex(models.Model):
+    ticker = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'market_indices'
+
+    def __str__(self):
+        return self.ticker
+
+
+class ValueUpdate(models.Model):
+    market_index = models.ForeignKey(MarketIndex, on_delete=models.CASCADE)
+    current_value = models.DecimalField(max_digits=10, decimal_places=2)
+    points_change = models.DecimalField(max_digits=10, decimal_places=2)
+    percent_change = models.DecimalField(max_digits=5, decimal_places=2)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.market_index.name}: {self.updated_at.isoformat()}'
