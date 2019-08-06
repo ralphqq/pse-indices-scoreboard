809d4db496ca02c07bad54c8ce3dc630d73f8575
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-08-06 12:30:01 +0800

    Renamed scrapy settings file to config.py and set allowed_domains in spider class and added items to .gitignore

--- index_scraper/index_scraper/spiders/indices.py
+++ index_scraper/index_scraper/spiders/indices.py
@@ -1,11 +1,52 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+import json
+
+import pytz
 import scrapy
 
 
 class IndicesSpider(scrapy.Spider):
     name = 'indices'
-    allowed_domains = ['www.pse.com.ph/stockMarket/home.html']
-    start_urls = ['http://www.pse.com.ph/stockMarket/home.html/']
+    allowed_domains = ['www.pse.com.ph']
+    start_urls = ['http://www.pse.com.ph/stockMarket/home.html']
+    headers = {
+        'Host': 'www.pse.com.ph',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Referer': 'https://www.pse.com.ph/stockMarket/home.html',
+        'X-Requested-With': 'XMLHttpRequest',
+        'DNT': '1',
+        'Connection': 'keep-alive'
+    }
 
     def parse(self, response):
-        pass
+        yield scrapy.Request(
+            url=self.get_url(),
+            method='GET',
+            headers=self.headers,
+            callback=self.parse_object
+        )
+
+    def parse_object(self, response):
+        records = json.loads(response.text).get('records')
+        if not records:
+            raise ValueError('No data obtained')
+
+        for record in records:
+            yield {
+                'name': record['indexName'],
+                'current_value': record['indexPoints'],
+                'points_change': record['changeValue'],
+                'percent_change': record['percentageChange'],
+                'market_status': record['marketStatus']
+            }
+
+    def get_url(self):
+        utc_now = pytz.utc.localize(datetime.utcnow())
+        phl_now = utc_now.astimezone(pytz.timezone('Asia/Manila'))
+        ts = int(phl_now.timestamp()) * 1000
+        return f'https://www.pse.com.ph/stockMarket/dailySummary.html?method=getMarketIndices&ajax=true&_dc={ts}'
+
